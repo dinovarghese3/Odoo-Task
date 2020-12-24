e60b4406ead73bb0fff40ccd00b67c527eeaba8a
dinovarghese3 <dinovarghese3@gmail.com>
2020-12-24 16:51:03 +0530

    Initial commit - Added some models field

--- vechicle_rental/__manifest__.py
+++ vechicle_rental/__manifest__.py
@@ -15,7 +15,7 @@
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'fleet'],
 
     # always loaded
     'data': [
--- vechicle_rental/models/models.py
+++ vechicle_rental/models/models.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
-from odoo.addons import fleet
 from odoo import models, fields, api
+from odoo.tools.safe_eval import datetime
 
 
 class VehicleRental(models.Model):
     _name = 'vehicle.rental'
     _description = " Vehicle rental app"
+    _inherit =
 
     print("HAi NEXT CHECK")
-    vehicle = fields.Many2one('fleet.vehicle','vehicle')
-    vehicle_type = fields.Char(string='vehicle Type')
+    vehicle = fields.Selection([('fleet.vehicle','car')],'vehicle')
     name = fields.Char(string='Name', required=True)
+    brand = fields.Char(string='Brand')
+    registration_date = fields.Date('Registration Date ', required=False,
+                                    help='Date when the vehicle has been Register')
+    model = fields.Char(string='model')
+    rent = fields.Float(string="Rent", digits=(6, 2))
