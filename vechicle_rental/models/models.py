927442575f19287527ecec55551317f756413725
dinovarghese3 <dinovarghese3@gmail.com>
2020-12-28 08:29:36 +0530

    Many2onefield added

--- vechicle_rental/models/models.py
+++ vechicle_rental/models/models.py
@@ -6,13 +6,16 @@ from odoo.tools.safe_eval import datetime
 class VehicleRental(models.Model):
     _name = 'vehicle.rental'
     _description = " Vehicle rental app"
-    _inherit =
 
     print("HAi NEXT CHECK")
-    vehicle = fields.Selection([('fleet.vehicle','car')],'vehicle')
+    vehicle = fields.Many2one('fleet.vehicle', string='Vehicle')
     name = fields.Char(string='Name', required=True)
     brand = fields.Char(string='Brand')
     registration_date = fields.Date('Registration Date ', required=False,
                                     help='Date when the vehicle has been Register')
     model = fields.Char(string='model')
     rent = fields.Float(string="Rent", digits=(6, 2))
+
+    # @api.onchange('model_id')
+    # def on_change_model_id(self):
+    #      brand = self.brand_id
