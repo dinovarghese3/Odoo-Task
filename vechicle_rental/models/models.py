4e638f8febf78195592233097351721b9286e035
dinovarghese3 <dinovarghese3@gmail.com>
2020-12-31 14:51:32 +0530

    1st task completed

--- vechicle_rental/models/models.py
+++ vechicle_rental/models/models.py
@@ -9,27 +9,22 @@ class VehicleRental(models.Model):
     vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle',
                                  domain=[('state_id', '=', 3)])
     name = fields.Char(string='Name', required=True)
-    brand = fields.Char(string='Brand')
+    brand_id = fields.Many2one(string='Brand',
+                               related='vehicle_id.brand_id', store=True)
+    print(brand_id)
     registration_date = fields.Date('Registration Date ', required=False,
-                                    help='Date the vehicle has Register')
+                                    help='Date the vehicle has Register',
+                                    readonly=True,
+                                    related='vehicle_id.registration_date')
     model = fields.Char(string='Model')
     currency_id = fields.Many2one('res.currency', string='Currency',
-                                  default=lambda self: self.env.user.company_id.id)
+                                  default=lambda
+                                      self: self.env.user.company_id.id)
     rent = fields.Monetary(string='Rent')
     state = fields.Selection(
         [('available', 'Available'), ('notavailable', 'Not available'),
          ('sold', 'Sold')],
         string='State', default='available')
-
-    # def on_change_model_id(self):
-    #      brand = self.brand_id
-    @api.onchange('vehicle_id')
-    def onchange_vehicle(self):
-        for rec in self:
-            if rec.vehicle_id:
-                rec.registration_date = rec.vehicle_id.registration_date
-                # rec.brand='fleet.vehicle.model'.rec.vehicle_id.brand_id
-                print(rec.vehicle_id)
 
 
 class RegisterDate(models.Model):
