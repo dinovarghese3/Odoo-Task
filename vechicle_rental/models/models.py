d831b8f108c9a90976ab39656d5f682eff7aad9d
dinovarghese3 <dinovarghese3@gmail.com>
2021-01-04 21:48:13 +0530

    Task3 started added Smart button and pages

--- vechicle_rental/models/models.py
+++ vechicle_rental/models/models.py
@@ -1,35 +1,85 @@
 # -*- coding: utf-8 -*-
+""" Vehicle Rents"""
 from odoo import models, fields, api
 from odoo.tools.safe_eval import datetime
 
 
 class VehicleRental(models.Model):
+    """Class contain all Vehicle details"""
     _name = 'vehicle.rental'
     _description = " Vehicle rental app"
 
     vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle',
                                  domain=[('state_id', '=', 3)])
-    name = fields.Many2one(string='Name', required=True,related='vehicle_id.brand_id')
+    name = fields.Char(string='Name', compute='_compute_name',
+                       related='vehicle_id.name',
+                       store=True)
     brand_id = fields.Many2one(string='Brand',
                                related='vehicle_id.brand_id', store=True)
-    print(brand_id)
     registration_date = fields.Date('Registration Date ', required=False,
                                     help='Date the vehicle has Register',
                                     readonly=True,
                                     related='vehicle_id.registration_date')
-
-    model = fields.Date(string='Model',related='vehicle_id.registration_date')
+    model = fields.Char(string='Model')
     currency_id = fields.Many2one('res.currency', string='Currency',
                                   default=lambda
-                                      self: self.env.user.company_id.id)
+                                      self: self.env.user.company_id.currency_id)
+
     rent = fields.Monetary(string='Rent')
     state = fields.Selection(
         [('available', 'Available'), ('notavailable', 'Not available'),
          ('sold', 'Sold')],
         string='State', default='available')
+    all_request = fields.One2many('vehicle.request', 'vehicle_id',
+                                  string='All Requests',
+                                  domain=lambda self: [
+                                      ('state', '=', 'confirm')])
+    rent_charges = fields.One2many('rent.charges', 'vehicle_id')
+
+    def all_rental_requests(self):
+        """ Function for Smart button"""
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Requests',
+            'view_mode': 'tree',
+            'res_model': 'vehicle.request',
+            'domain': [('vehicle_id', '=', self.name)],
+            'context': "{'create': False}"
+        }
+
+    @api.onchange('registration_date')
+    def _onchange_registration_date(self):
+        """ Calculating Year from registration date """
+        # print('contry', self.env.user.company_id.id)
+        # print(self.registration_date)
+        if self.registration_date:
+            self.model = str((datetime.datetime.strptime(
+                str(self.registration_date), "%Y-%m-%d")).year)
+
+
+class RentCharges(models.Model):
+    """Amount calculation based on date"""
+    _name = 'rent.charges'
+    vehicle_id = fields.Many2one('vehicle.rental', string="vehicle")
+    currency_id = fields.Many2one('res.currency', string='Currency',
+                                  default=lambda
+                                      self: self.env.user.company_id.currency_id)
+    amount = fields.Monetary(string='Amount')
+    period = fields.Float(strig='Period', default=1)
+    time = fields.Selection([('hour', 'Hour'), ('day', 'Day'), ('week', 'Week'),
+                             ('month', 'Month')], string="Time", default='day')
+
+    @api.onchange('period')
+    def _onchange_time(self):
+        if self.time is 'day':
+            self.amount = self.period * self.vehicle_id.rent
+            print(self.vehicle_id.rent)
+            print(self.amount)
+            print(self.period)
 
 
 class RegisterDate(models.Model):
+    """ Adding new field registration date to fleet module """
     _inherit = 'fleet.vehicle'
 
     registration_date = fields.Date('Registration Date ', required=False,
--- vechicle_rental/models/rent_request.py
+++ vechicle_rental/models/rent_request.py
@@ -1,11 +1,11 @@
+""" rent request"""
 from datetime import datetime
-
-from addons.hw_escpos.escpos import exceptions
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
 class RentRequest(models.Model):
+    """Rent request model"""
     _name = 'vehicle.request'
     _description = "All Rent requests"
     _rec_name = 'sequence'
@@ -17,7 +17,7 @@ class RentRequest(models.Model):
     request_date = fields.Date(string="Request Date", default=datetime.today())
     vehicle_id = fields.Many2one('vehicle.rental', string="Vehicle",
                                  domain=[('state', '=', 'available')],
-                                 required=True)
+                                 required=True,force_create=False)
     from_date = fields.Date(string="From Date")
     to_date = fields.Date(string="To Date")
     period = fields.Integer(string="Period")
@@ -30,28 +30,37 @@ class RentRequest(models.Model):
                                       self: self.env.user.company_id.id)
     rent = fields.Monetary(string="Rent", related='vehicle_id.rent')
 
-    def action_confirm(self):
-        print("hai")
+    def button_confirm(self):
+        """ Button confirm """
         for rec in self:
-            rec.state = 'confirm'
+            rec.write({'state': 'confirm'})
+            rec.vehicle_id.write({'state': 'notavailable'})
+
+    def button_return(self):
+        """Button return"""
+        for rec in self:
+            rec.write({'state': 'return'})
+            rec.vehicle_id.write({'state': 'available'})
 
     @api.model
     def create(self, vals):
+        """ Fuction to create sequnce """
         if vals.get('sequence', 'New') == 'New':
             vals['sequence'] = self.env['ir.sequence'].next_by_code(
                 'vehicle.request.sequence') or 'New'
-        result = super(RentRequest, self).create(vals)
+        result = super().create(vals)  # super(RentRequest, self) same
         return result
 
     @api.onchange('from_date', 'to_date')
     def _onchange_from_date_to_date(self):
+        """Period calculation"""
         if self.from_date and self.to_date:
-            for rec in self:
-                if rec.from_date < rec.to_date:
-                    rec.period = (rec.to_date - rec.from_date).days
+            if self.from_date < self.to_date:
+                self.period = (self.to_date - self.from_date).days
 
     @api.constrains('from_date', 'to_date')
     def _constrain_from_date_to_date(self):
+        """Date Validation"""
         if self.from_date and self.to_date:
             for rec in self:
                 if rec.from_date > rec.to_date:
