3364709b4cca0980b9eb32346a482f985e61a5ce
dinovarghese3 <dinovarghese3>
2021-02-01 16:49:45 +0530

    Added Brand name in Orderline odoo14

--- product_brand/__manifest__.py
+++ product_brand/__manifest__.py
@@ -17,18 +17,18 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '14.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'point_of_sale', 'product'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/product_brand_inherit_view.xml',
         'views/product_brand_js_view.xml',
 
     ],
     # only loaded in demonstration mode
-    'qweb':['static/src/xml/pos_product_brand.xml'],
+    'qweb':['static/src/xml/pos_product_brand.xml',],
 }
--- product_brand/models/product_brand_inherit.py
+++ product_brand/models/product_brand_inherit.py
@@ -3,9 +3,15 @@
 from odoo import models, fields, api
 
 
+class ProductBrandNameInherit(models.Model):
+    _name = 'productbrand.name'
+    name = fields.Char("Brand")
+
+
 class ProductBrandInherit(models.Model):
     # _name = 'product.inherit.brand'
     _inherit = 'product.product'
     _description = 'Product Brand to POS'
+    _rec_name = 'name'
 
-    brand_name = fields.Char(string="Brand")
+    brand_name_id= fields.Many2one('productbrand.name', string="Brand")
