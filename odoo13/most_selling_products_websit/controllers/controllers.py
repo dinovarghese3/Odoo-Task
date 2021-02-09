148a633b98d1085f905b104d0986c9bb1ac51061
dinovarghese3 <dinovarghese3>
2021-02-09 17:45:26 +0530

    Added ui for website

--- odoo13/most_selling_products_websit/controllers/controllers.py
+++ odoo13/most_selling_products_websit/controllers/controllers.py
@@ -6,6 +6,9 @@ class MostSale(http.Controller):
     @http.route('/products', type='http', auth='public', website=True)
     def most_sale_detsils(self, **kwargs):
         print("hai")
-        # sale_details = request.env['sale.order'].sudo().search([])
-        # return request.render('most_selling_products_websit.most_product',
+        products = request.env['product.template'].sudo().search(
+            [('sale_ok', '=', True)])
+        return request.render('most_selling_products_websit.product',
+                              {'products': products})
         #                       {'my_details': sale_details})
+        # return "Hai Dino"
