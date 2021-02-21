52cd3561eb9454477737acc43d41d923dff0743b
dinovarghese3 <dinovarghese3>
2021-02-21 20:55:09 +0530

    Completed website task2 leave requst

--- portal_leave/controllers/controllers.py
+++ portal_leave/controllers/controllers.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
-
-from odoo import http, fields
+""" Leave request creation from website  """
+from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import QueryURL
 
 
 class PortalLeave(http.Controller):
+    """Showing All leave request created"""
+
     @http.route('/leave', auth='public', website=True)
-    def index(self, **kw):
+    def index(self):
         """ Showing All the leaves created """
 
         all_leaves = request.env['hr.leave'].sudo().search(
             [('user_id', '=', request.env.uid)])
-        print(all_leaves)
-        keep = QueryURL()
-        print("LLL", keep)
-        values = {}
+        # print(all_leaves)
+        keep = QueryURL()  # For sending value in url
+        # print("LLL", keep)
         return request.render('portal_leave.leave_request_portal',
                               {'all_leaves': all_leaves, 'keep': keep})
 
     @http.route('/leave/delete', auth='public', type='http', website=True,
                 csrf=False)
-    def _onclick_delete(self, id=None, **kw):
+    def _onclick_delete(self, id=None):
         """ Cancel button Click """
         if id:
-            print(id)
+            # id from Url
+            # print(id)
             request.env['hr.leave'].sudo().search(
                 [('id', '=', id)]).unlink()
         vals = "Record Deleted"
@@ -32,107 +34,128 @@ class PortalLeave(http.Controller):
                               {'vals': vals})
 
 
-""" Leave Request Creation Form Class"""
-
-
 class RequestLeave(http.Controller):
+    """ Leave Request Creation Form Class"""
+    request_hour_from = [
+        ('0', '12:00 AM'), ('0.5', '12:30 AM'),
+        ('1', '1:00 AM'), ('1.5', '1:30 AM'),
+        ('2', '2:00 AM'), ('2.5', '2:30 AM'),
+        ('3', '3:00 AM'), ('3.5', '3:30 AM'),
+        ('4', '4:00 AM'), ('4.5', '4:30 AM'),
+        ('5', '5:00 AM'), ('5.5', '5:30 AM'),
+        ('6', '6:00 AM'), ('6.5', '6:30 AM'),
+        ('7', '7:00 AM'), ('7.5', '7:30 AM'),
+        ('8', '8:00 AM'), ('8.5', '8:30 AM'),
+        ('9', '9:00 AM'), ('9.5', '9:30 AM'),
+        ('10', '10:00 AM'), ('10.5', '10:30 AM'),
+        ('11', '11:00 AM'), ('11.5', '11:30 AM'),
+        ('12', '12:00 PM'), ('12.5', '12:30 PM'),
+        ('13', '1:00 PM'), ('13.5', '1:30 PM'),
+        ('14', '2:00 PM'), ('14.5', '2:30 PM'),
+        ('15', '3:00 PM'), ('15.5', '3:30 PM'),
+        ('16', '4:00 PM'), ('16.5', '4:30 PM'),
+        ('17', '5:00 PM'), ('17.5', '5:30 PM'),
+        ('18', '6:00 PM'), ('18.5', '6:30 PM'),
+        ('19', '7:00 PM'), ('19.5', '7:30 PM'),
+        ('20', '8:00 PM'), ('20.5', '8:30 PM'),
+        ('21', '9:00 PM'), ('21.5', '9:30 PM'),
+        ('22', '10:00 PM'), ('22.5', '10:30 PM'),
+        ('23', '11:00 PM'), ('23.5', '11:30 PM')]
+    index_of_to = 0.0
+    index_of_from = 0.0
+
     @http.route('/leave/request', auth='public', website=True)
     def index(self, **kw):
         """ Leave request Creation Form"""
         leave_type = request.env['hr.leave.type'].sudo().search([])
         # custom_hour=request.env['hr.leave'].sudo().search([])
-        request_hour_from = [
-            ('0', '12:00 AM'), ('0.5', '12:30 AM'),
-            ('1', '1:00 AM'), ('1.5', '1:30 AM'),
-            ('2', '2:00 AM'), ('2.5', '2:30 AM'),
-            ('3', '3:00 AM'), ('3.5', '3:30 AM'),
-            ('4', '4:00 AM'), ('4.5', '4:30 AM'),
-            ('5', '5:00 AM'), ('5.5', '5:30 AM'),
-            ('6', '6:00 AM'), ('6.5', '6:30 AM'),
-            ('7', '7:00 AM'), ('7.5', '7:30 AM'),
-            ('8', '8:00 AM'), ('8.5', '8:30 AM'),
-            ('9', '9:00 AM'), ('9.5', '9:30 AM'),
-            ('10', '10:00 AM'), ('10.5', '10:30 AM'),
-            ('11', '11:00 AM'), ('11.5', '11:30 AM'),
-            ('12', '12:00 PM'), ('12.5', '12:30 PM'),
-            ('13', '1:00 PM'), ('13.5', '1:30 PM'),
-            ('14', '2:00 PM'), ('14.5', '2:30 PM'),
-            ('15', '3:00 PM'), ('15.5', '3:30 PM'),
-            ('16', '4:00 PM'), ('16.5', '4:30 PM'),
-            ('17', '5:00 PM'), ('17.5', '5:30 PM'),
-            ('18', '6:00 PM'), ('18.5', '6:30 PM'),
-            ('19', '7:00 PM'), ('19.5', '7:30 PM'),
-            ('20', '8:00 PM'), ('20.5', '8:30 PM'),
-            ('21', '9:00 PM'), ('21.5', '9:30 PM'),
-            ('22', '10:00 PM'), ('22.5', '10:30 PM'),
-            ('23', '11:00 PM'), ('23.5', '11:30 PM')]
 
         # custom_hour_typ=custom_hour.request_hour_from
-        leave = request.env['hr.leave'].name
-        print(leave)
+        # leave = request.env['hr.leave'].name
+        # print(leave)
         # selection=leave.request_hour_from
         # print(selection)
         return request.render('portal_leave.request_leave_apply',
                               {'leave_type': leave_type,
-                               'request_hour_from': request_hour_from,
+                               'request_hour_from': self.request_hour_from,
                                })
 
     @http.route('/leave/request/submit', type='http', auth='public',
                 website=True, csrf=False)
     def leave_request_submition(self, **post):
         """ Submit Button Press. Create a leave """
-        curent = fields.Many2one('hr.employee', "current_user",
-                                 default=lambda self: self.env.user)
-        print(curent)
+        # curent = fields.Many2one('hr.employee', "current_user",
+        #                          default=lambda self: self.env.user)
+        # print(curent)
         current_user = request.env['hr.employee'].sudo().search(
-            [('user_id', '=', request.env.uid)])
-        print(current_user.name)
+            [('user_id', '=',
+              request.env.uid)])  # getting current user loged in
+        # print("current user", current_user.name)
         leave_type = request.env['hr.leave.type'].sudo().search([])
-        print(current_user.name)
-        print(current_user)
-        print(post)
         alert_date = ""
-        # leave_type = post.get('leave_type')
-        # print(leave_type)
+        duration = 0
         leave_id = request.env['hr.leave.type'].sudo().search(
-            [('name', '=', post.get('leave_type'))])
-        print(leave_id.id)
+            [('name', '=', post.get(
+                'leave_type'))])  # geting the leave type the user selected
+        # print("leave id", leave_id.id)
         if post.get('half_day'):
-            duration = 0
+            # checking the Half day check box is enabled or not if enabled set
+            # the duration as 4 and set the start date and end date same
+            duration = 4
+            # print("duration half_day :", duration)
             end_date = post.get('start_date')
         else:
             end_date = post.get('end_date')
-        if post.get('#custom_hours'):
-            print("hai", post.get('#leave_type_to'))
-            print(post.get('#leave_type_from'))
+        if post.get('custom_hours'):
+            # checking the custom hours in checked or not
+            # print("leave type to :", post.get('leave_type_to'))
+            # print("leave type from :", post.get('leave_type_from'))
+            # duration = post.get('leave_type_from') - post.get('leave_type_to')
             duration = 0
-            end_date = post.get('start_date')
+            # print("looop")
+            # for i in self.filtered(
+            #         lambda l: l[1] == post.get('leave_type_from')):
+            #     print("i :", i[0])
+
+            for i in self.request_hour_from:
+                if i[1] == post.get('leave_type_to'):
+                    self.index_of_to = i[0]
+                    print("to", self.index_of_to)
+                if i[1] == post.get('leave_type_from'):
+                    self.index_of_from = i[0]
+                    print("from", self.index_of_from)
+            duration = float(self.index_of_from) - float(self.index_of_to)
+            print("duration custom_hour", duration, self.index_of_from,
+                  self.index_of_to)
+            end_date = post.get(
+                'start_date')  # seting the end date same as start date
         else:
-            duration = post.get('#duration')
-        print(end_date)
-        print(post.get('start_date'))
-        if not end_date:
-            end_date = post.get('start_date')
+            duration = post.get('duration')
+        print("end_date :", end_date)
+        print("start date :", post.get('start_date'))
+        # if not end_date:  # in case end date missing
+        #     end_date = post.get('start_date')
+        print("duration last", duration)
+        if not self.index_of_from and not self.index_of_to:
+            self.index_of_from = 0.0
+            self.index_of_to = 0.0
         if (post.get('start_date') and end_date and post.get(
-                'start_date') <= end_date) or post.get('#custom_hours'):
+                'start_date') <= end_date) or post.get('custom_hours'):
             leave_request = request.env['hr.leave'].sudo().create({
                 'holiday_status_id': leave_id.id,
                 'request_date_from': post.get('start_date'),
-                'request_date_to': post.get('start_date'),
+                'request_date_to': end_date,
                 'number_of_days': duration,
                 'name': post.get('description'),
-                'request_date_from_period': post.get('#time_period'),
+                'request_date_from_period': post.get('time_period'),
                 'request_unit_hours': post.get('custom_hours'),
                 'request_unit_half': post.get('half_day'),
-                'request_hour_from': post.get('#leave_type_from'),
-                'request_hour_to': post.get('#leave_type_to'),
+                'request_hour_from': self.index_of_from,
+                'request_hour_to': self.index_of_to,
                 'employee_id': current_user.id,
                 'holiday_type': "employee",
                 'department_id': current_user.department_id.id,
             })
-            print(leave_request)
-            # vals = {'leave_request': leave_request, }
-            # print(vals)
             vals = " Leave Created"
             return request.render('portal_leave.leave_request_form_success',
                                   {'vals': vals,
--- portal_leave/static/src/js/leave_form_frontend.js
+++ portal_leave/static/src/js/leave_form_frontend.js
@@ -1,32 +1,35 @@
 odoo.define('portal_leave.leave_form_frontend', function(require) {
 
-
     $('#half_day_leave_type').hide();
     $('#time_period').hide()
     $('#leave_type_time_off_from,#leave_type_time_off_to,#leave_type_time_off_label').hide();
     console.log("Hai")
     $('#leave_type_time_off_from_other_model').change(function(){
-//    Leave Time off checking Compensatory days or unpaid. if unpaid or
-//Compensatory days then half_day,custom_hours check box will be enabled and end
-// date disabled
-    if (($('#leave_type_time_off_from_other_model').val() == 'Compensatory Days')||($('#leave_type_time_off_from_other_model').val() == 'Unpaid')){
-        $('#half_day_leave_type').show();
-        $('#end_date_leave').show();
-        $('custom_hours').show()
-    }
-    else{
-        $('#half_day_leave_type').hide();
-        $('#end_date_leave').show();
-        $('custom_hours').hide()
-    }
-});
+
+        if (($('#leave_type_time_off_from_other_model').val() == 'Compensatory Days')||($('#leave_type_time_off_from_other_model').val() == 'Unpaid')){
+            $('#half_day_leave_type').show();
+            $('#end_date_leave').show();
+            $('custom_hours').show()
+        }
+        else{
+            $('#half_day_leave_type').hide();
+            $('#end_date_leave').show();
+            $('custom_hours').hide()
+        }
+    });
 
    //Compute Duration based on start date and end date
     $('#start_date_leave,#end_date_leave').change(function(){
-    var diff =new Date($('#end_date_leave').val()) - new Date($('#start_date_leave').val());
-    var no_of=(diff/1000/60/60/24);
-    console.log("LL",no_of);
-    $('#duration').val(no_of+1);
+
+        var diff =new Date($('#end_date_leave').val()) - new Date($('#start_date_leave').val());
+        var no_of=(diff/1000/60/60/24);
+        console.log("LL",no_of);
+        if (no_of <0){
+            alert("End date must be grater than start date");
+            $('#duration').val(0);
+        }
+        else{$('#duration').val(no_of+1);}
+
     });
 
 //  Checking thr half_day check box is checked or not if checked end date,duration hided and time peroid enabled
@@ -38,20 +41,23 @@ odoo.define('portal_leave.leave_form_frontend', function(require) {
                 $('#custom_hours').prop("checked", false);
                 $('#time_period').show();
                 $('#leave_type_time_off_from,#leave_type_time_off_to,#leave_type_time_off_label').hide();
-                $('#duration_leave_label,#duration').hide();
+//                $('#duration_leave_label,#duration').hide();
+//                $('#unit_of_duration').hide()
+                 $('#unit_of_duration').html("Hours")
+                 $('#duration').val("4");
             }
             else if($(this).prop("checked") == false){
                 console.log("Checkbox is unchecked.");
                 $('#time_period').hide();
                 $('#end_date_leave,#end_date_label').show();
                 $('#duration_leave_label,#duration').show();
-
+                $('#unit_of_duration').show();
+                $('#unit_of_duration').html("Days")
             }
-            });
-
+    });
 
 //            Checking Custom hours checked or not
-$('input[id="custom_hours"]').click(function(){
+    $('input[id="custom_hours"]').click(function(){
             if($(this).prop("checked") == true){
                 console.log("Checkbox is checked.");
                 $('#end_date_leave').val($('#start_date_leave').val())
@@ -59,7 +65,8 @@ $('input[id="custom_hours"]').click(function(){
                 $('#end_date_leave,#end_date_label').hide();
                 $('#time_period').hide();
                 $('#leave_type_time_off_from,#leave_type_time_off_to,#leave_type_time_off_label').show();
-                $('#duration_leave_label,#duration').show();
+                $('#duration_leave_label,#duration').hide();
+                $('#unit_of_duration').hide();
             }
             else if($(this).prop("checked") == false){
                 console.log("Checkbox is unchecked.");
@@ -67,14 +74,23 @@ $('input[id="custom_hours"]').click(function(){
                 $('#leave_type_time_off_from,#leave_type_time_off_to,#leave_type_time_off_label').hide();
                 $('#end_date_leave,#end_date_label').show();
                 $('#duration_leave_label,#duration').show();
+                $('#unit_of_duration').html("Days")
             }
-            });
+    });
 
-$('#leave_type_from,#leave_type_to').change(function(){
-    console.log("leave tyoe fun")
-    console.log($('#leave_type_to'))
-    console.log($('#leave_type_from'))
-    $('duration').val(($('#leave_type_to').val()-$('#leave_type_from').val()))
-})
+    $('#leave_type_from,#leave_type_to').change(function(){
+        console.log("leave tyoe fun")
+        console.log($('#leave_type_from').val())
+        console.log($('#leave_type_to').val())
+        var duration = $('#leave_type_to').val()
+        $('duration').val($('#leave_type_to').val())
+    });
+//    $('#submit_button').click(function(event){
+//        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+//        if( ('#duration').val() <=0 ){
+//            alert("Check your Dates");
+//        }
+//
+//    })
 
 })
